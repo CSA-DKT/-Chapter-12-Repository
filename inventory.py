8e21bbe050282f202e841083a96fa0b0462dc303
CSA-DKT <33467694+CSA-DKT@users.noreply.github.com>
2018-03-09 15:42:05 -0600

    This is a program where I had to the longest word in Alice and Wonderland and how many characters it has.

---
+++ inventory.py
@@ -0,0 +1,6 @@
+inventory= {'lime': 11, 'cherries': 23, 'bannanas': 24, 'blueberies': 30}
+print(inventory['lime'])
+print(inventory['cherries'])
+print(inventory['bannanas'])
+print(inventory['blueberies'])
+   
